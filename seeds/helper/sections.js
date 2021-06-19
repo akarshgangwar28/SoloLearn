3c1c03528abde1d6c65007de01d72a90a2d57444
akarshgangwar28 <akarshprog28@gmail.com>
2021-06-19 19:48:02 +0530

    Update sections.js

--- seeds/helper/sections.js
+++ seeds/helper/sections.js
@@ -1,13 +1,16 @@
 const Videos = require('../../models/videos');
-
+var v = []
 module.exports.generateRandomVideos = () => {
-    var v = []
+    
     const randomVideosNo = Math.floor(Math.random() * 10);
     for (let j = 0; j < randomVideosNo; j++) {
         const randomVideo = Math.floor(Math.random() * 48) + 1;
         Videos.findOne().skip(randomVideo).exec((err, result) => {
             v.push(String(result._id));
+            // console.log(v)
+
         })
+
     }
     return v;
 }
